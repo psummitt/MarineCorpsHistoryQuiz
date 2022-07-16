e3f6a50db2b50d569dba10a0ca1a4d47d62743e5
psummitt <psummitt@gmail.com>
2022-07-16 13:29:48 -0500

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,26 @@
 const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
     startButton.classList.add('hide')
+    shuffleQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
     setNextQuestion()
 }
 
 function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
+function showQuestion(question){
+    questionElement.innerText = question.question
 }
 
 function selectAnswer() {
